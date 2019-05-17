30ac70cad46b78ac47fa1036437a0040cb9715ed
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-17 14:35:45 +0300

    Условные выражения, Условные конструкции

---
+++ Глава 1/10. Условные конструкции/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Условные_конструкции
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            int num2 = 6;
+
+            /**
+             * Конструкция if/else проверяет истинность некоторого условия
+             * и в зависимости от результатов проверки выполняет определенный код
+             */
+            if (num1 > num2)
+            {
+                Console.WriteLine($"Число {num1} больше числа {num2}");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine($"Число {num1} меньше числа {num2}");
+            }
+            else
+            {
+                Console.WriteLine("Число num1 равно числу num2");
+            }
+
+            int num3 = 8;
+            int num4 = 6;
+
+            if (num3 > num4 && num3 == 8)
+            {
+                Console.WriteLine($"Число {num1} больше числа {num2}");
+            }
+
+            int number = 1;
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("case 1");
+                    goto case 5; // переход к case 5
+                case 3:
+                    Console.WriteLine("case 3");
+                    break;
+                case 5:
+                    Console.WriteLine("case 5");
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+
+            // Тернарную операция имеет следующий синтаксис: [первый операнд - условие] ? [второй операнд] : [третий операнд].
+            int x = 3;
+            int y = 2;
+            Console.WriteLine("Нажмите + или -");
+            string selection = Console.ReadLine();
+
+            int z = selection == "+" ? (x + y) : (x - y);
+            Console.WriteLine(z);
+        }
+    }
+}
---
+++ Глава 1/9. Условные выражения/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _9.Условные_выражения
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Операции сравнения
+            int a = 10;
+            int b = 4;
+
+            bool c = a == b; // false
+            bool c2 = a != b; // true
+            bool c3 = a < b; // false
+            bool c4 = a > b; // true
+            bool c5 = a <= b; // false
+            bool c6 = a >= b; // true
+
+            // Логические операции
+            /*
+             * |, & - вычисляют все операции.
+             * ||, && - вычисляют до необходимой операции, в результате которой будет инвестно решение.
+             */
+            bool x1 = (5 > 6) | (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+            bool x2 = (5 > 6) | (4 > 6); // 5 > 6 - false, 4 > 6 - false, поэтому возвращается false
+
+            bool x3 = (5 > 6) & (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается false
+            bool x4 = (5 < 6) & (4 < 6); // 5 < 6 - true, 4 < 6 - true, поэтому возвращается true
+
+            bool x5 = (5 > 6) || (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+            bool x6 = (5 > 6) || (4 > 6); // 5 > 6 - false, 4 > 6 - false, поэтому возвращается false
+
+            bool x7 = (5 > 6) && (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается false
+            bool x8 = (5 < 6) && (4 < 6); // 5 < 6 - true, 4 > 6 - true, поэтому возвращается true
+
+            bool x9 = true;
+            bool x10 = !x9; // false
+
+            bool x11 = (5 > 6) ^ (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+            bool x12 = (50 > 6) ^ (4 / 2 < 3); // 50 > 6 - true, 4/2 < 3 - true, поэтому возвращается false
+        }
+    }
+}
