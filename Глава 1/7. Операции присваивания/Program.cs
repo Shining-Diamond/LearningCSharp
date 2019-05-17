49cd4990a159eb2a21063f75d04de477fa5174c2
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-17 05:27:19 +0300

    Операции присваивания, Преобразования базовых типов данных

---
+++ Глава 1/7. Операции присваивания/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _7.Операции_присваивания
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            number += 10; // 20
+            number -= 4; // 16
+            number *= 2; // 32
+            number /= 8; // 4
+            number <<= 4; // 100 -> 1000000 => 64
+            number >>= 2; // 1000000 -> 10000 => 16
+
+            // Операции присвоения выполняются справа налево
+            int a = 8;
+            int b = 6;
+            int c = a += b -= 5; // 9
+        }
+    }
+}
