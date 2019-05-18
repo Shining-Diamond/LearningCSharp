ac3bb6180b94f72daf815dd104795cf0e2f52d7a
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-19 01:40:48 +0300

    Методы, Параметры методов

---
+++ Глава 1/14. Методы/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _14.Методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывод сообщения Hello
+            SayHello();
+
+            // Вывод сообщения GoodBye
+            SayGoodbye();
+
+            string message = GetHello();
+
+            Console.WriteLine(message); // Hello
+
+            int summa = GetSum();
+
+            Console.WriteLine(summa); // 5
+
+            Console.ReadKey();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static void SayGoodbye()
+        {
+            Console.WriteLine("GoodBye");
+        }
+
+        static string GetHello()
+        {
+            return "Hello";
+        }
+
+        /// <summary>
+        /// Получаем сумму чисел 2 и 3
+        /// </summary>
+        /// <returns></returns>
+        static int GetSum()
+        {
+            int x = 2;
+            int y = 3;
+            return x + y;
+        }
+
+        // Сокращенная запись метода
+        static void SayHelloMiniman() => Console.WriteLine("Hello");
+    }
+}
---
+++ Глава 1/15. Параметры методов/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Параметры_методов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 35;
+            int result = Sum(a, b);
+
+            Console.WriteLine(result); // 60
+
+            result = Sum(a + b + 12, 18); // "a + b + 12" представляет значение параметра x
+
+            Console.WriteLine(result); // 90
+
+            Display("Tom", 24); // Name: Tom  Age: 24
+
+            Console.WriteLine(OptionalParam(5, 20)); // 34
+
+            Console.WriteLine(OptionalParam(5, 5, 3, 2)); // 15
+
+            // Именованные параметры. Параметр z используется по умолчанию
+            Console.WriteLine(OptionalParam(y: 2, x: 3, s: 10)); // 20
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Суммирует два числа
+        /// </summary>
+        /// <param name="x">Первое число</param>
+        /// <param name="y">Второе число</param>
+        /// <returns>Сумму чисел</returns>
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// Отображает имя и возраст
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        static void Display(string name, int age)
+        {
+            Console.WriteLine($"Name: {name}  Age: {age}");
+        }
+
+        /// <summary>
+        /// Складывает числа x, y, z, s
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static int OptionalParam(int x, int y, int z = 5, int s = 4)
+        {
+            return x + y + z + s;
+        }
+    }
+}
