cdd38f438aee50b9bb1d92b8308edeff566eabb6
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-19 20:51:26 +0300

    Массив параметров и ключевое слово params, Область видимости (контекст) переменных

---
+++ Глава 1/17. Массив параметров и ключевое слово params/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _17.Массив_параметров_и_ключевое_слово_params
+{
+    class Program
+    {
+        // Передача params параметра
+        static void Addition(params int[] integers)
+        {
+            int result = integers.Sum();
+
+            Console.WriteLine(result);
+        }
+
+        // Передача массива
+        static void AdditionMas(int[] integers, int k)
+        {
+            int result = 0;
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                result += integers[i];
+            }
+
+            result *= k;
+
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            Addition(1, 2, 3, 4, 5);
+
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            AdditionMas(array, 2);
+
+            Console.ReadKey();
+        }
+    }
+}
