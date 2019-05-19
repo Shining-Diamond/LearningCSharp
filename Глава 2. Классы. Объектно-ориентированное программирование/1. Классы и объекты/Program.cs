298a7889c40a4fbba35219d9e86cf07da2643d7b
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-19 23:04:54 +0300

    Классы и объекты

---
+++ Глава 2. Классы. Объектно-ориентированное программирование/1. Классы и объекты/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Классы_и_объекты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
