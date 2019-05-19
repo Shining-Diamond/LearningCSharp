081a67ee5ea6987c391c59d9894de3057983050e
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-19 21:00:09 +0300

    Рекурсивные функции

---
+++ Глава 1/19. Рекурсивные функции/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _19.Рекурсивные_функции
+{
+    class Program
+    {
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        static int Fibonachi(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int fact5 = Factorial(5);
+
+            int fibonachi5 = Fibonachi(5);
+
+            Console.WriteLine(fact5); // 120
+            Console.WriteLine(fibonachi5); // 5
+
+            Console.ReadKey();
+        }
+    }
+}
