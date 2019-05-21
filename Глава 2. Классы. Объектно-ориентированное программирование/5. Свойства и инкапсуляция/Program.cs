1dcf0287f3fe05462b663a52b0960d2cc7af2c54
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-21 19:29:10 +0300

    Свойства и инкапсуляция

---
+++ Глава 2. Классы. Объектно-ориентированное программирование/5. Свойства и инкапсуляция/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _5.Свойства_и_инкапсуляция
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Tom", 24);
+
+            // Ошибка - set объявлен с модификатором private
+            //p.Name = "John";
+
+            Console.WriteLine(p.LastName); // Иванов
+
+            Console.WriteLine(p.Name); // Tom
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        private string name;
+        private int age;
+        private string lastName;
+
+        // эквивалентно public string LastName { get { return lastName; } }
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// Инициализация автоматического свойства значением по умолчанию.
+        /// </summary>
+        public int Money { get; set; } = 1000;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
