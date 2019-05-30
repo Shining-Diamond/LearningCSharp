e19b118f792702b0a49c2aee6245530bea6d48d7
rGillyazanov <rgillyazanov@yandex.ru>
2019-05-30 20:08:18 +0300

    Наследование

---
+++ Глава 2. Классы. Объектно-ориентированное программирование/12. Наследование/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _12.Наследование
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Bill");
+
+            p.Display();
+
+            Employee emp = new Employee("Tom", 22, "Microsoft");
+
+            emp.Display();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        string name;
+        int age;
+ 
+        public Person(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Person(string name)");
+        }
+
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+            Console.WriteLine("Person(string name, int age)");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    class Employee : Person
+    {
+        string company;
+ 
+        public Employee(string name, int age, string company) : base(name, age)
+        {
+            this.company = company;
+            Console.WriteLine("Employee(string name, int age, string company)");
+        }
+    }
+}
