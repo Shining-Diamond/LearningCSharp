54822273d7916952b5e448a5340344b660c2a099
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-06 13:45:31 +0300

    Добавлено виртуальное свойство

--- Глава 2. Классы. Объектно-ориентированное программирование/15. Виртуальные методы и свойства/Program.cs
+++ Глава 2. Классы. Объектно-ориентированное программирование/15. Виртуальные методы и свойства/Program.cs
@@ -9,10 +9,14 @@ namespace _15.Виртуальные_методы_и_свойства
             Person person1 = new Person("Игорь", "Иванов");
 
             person1.Display();
+            person1.Age = 25;
+            person1.DisplayAge();
 
             Employee person2 = new Employee("Петр", "Петров", "Google");
 
             person2.Display();
+            person2.Age = 30;
+            person2.DisplayAge();
 
             Console.ReadKey();
         }
@@ -22,6 +26,8 @@ namespace _15.Виртуальные_методы_и_свойства
             public string FirstName { get; set; }
 
             public string LastName { get; set; }
+
+            public virtual int Age { get; set; }
 
             public Person(string firstName, string lastName)
             {
@@ -33,11 +39,18 @@ namespace _15.Виртуальные_методы_и_свойства
             {
                 Console.WriteLine($"{FirstName} {LastName}");
             }
+
+            public virtual void DisplayAge()
+            {
+                Console.WriteLine($"{Age} лет");
+            }
         }
 
         class Employee : Person
         {
             public string Company { get; set; }
+
+            public override int Age { get => base.Age * 2; set => base.Age = value; }
 
             public Employee(string firstName, string lastName, string company)
                 : base(firstName, lastName)
@@ -51,6 +64,11 @@ namespace _15.Виртуальные_методы_и_свойства
             public override void Display()
             {
                 Console.WriteLine($"{FirstName} {LastName} работает в {Company}");
+            }
+
+            public sealed override void DisplayAge()
+            {
+                Console.WriteLine($"Если вы работаете в нашей компании, то вам скорее всего {Age}");
             }
         }
     }
