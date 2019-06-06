53dd79dcf94c8c27fd4810108df6814957f72071
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-06 19:24:41 +0300

    Сокрытие методов

---
+++ Глава 2. Классы. Объектно-ориентированное программирование/16. Сокрытие методов/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/**
+ * Также можно делать сокрытие для свойст и переменных
+ */
+
+namespace _16.Сокрытие_методов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bob = new Person("Bob", "Robertson");
+            bob.Display(); // Bob Robertson
+
+            Employee tom = new Employee("Tom", "Smith", "Microsoft");
+            tom.Display(); // Tom Smith работает в Microsoft
+
+            Console.ReadKey();
+        }
+
+        class Person
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public Person(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine($"{FirstName} {LastName}");
+            }
+        }
+
+        class Employee : Person
+        {
+            public string Company { get; set; }
+
+            public Employee(string firstName, string lastName, string company)
+                    : base(firstName, lastName)
+            {
+                Company = company;
+            }
+
+            public new void Display()
+            {
+                Console.WriteLine($"{FirstName} {LastName} работает в {Company}");
+            }
+        }
+    }
+}
