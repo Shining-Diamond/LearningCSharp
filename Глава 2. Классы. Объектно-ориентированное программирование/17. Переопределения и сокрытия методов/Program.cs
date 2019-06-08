1a7aeac51b5316eafa33e77d18648422ffac9620
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-08 21:33:58 +0300

    Разница между переопределением и сокрытием методов, Абстрактные классы и члены классов

---
+++ Глава 2. Классы. Объектно-ориентированное программирование/17. Переопределения и сокрытия методов/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _17.Переопределения_и_сокрытия_методов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Employee("Tom", "Smith", "Microsoft");
+            tom.Display();      // Tom Smith работает в Microsoft
+
+            /*
+             * Если сделать сокрытие метода Display в классе Employee, тогда будет вызван метод Display
+             * класса Person, так как он не переопределяется, а фактически создает новый метод в классе Employee
+             */
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+    }
+    class Employee : Person
+    {
+        public string Company { get; set; }
+
+        public Employee(string firstName, string lastName, string company)
+            : base(firstName, lastName)
+        {
+            Company = company;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName} работает в {Company}");
+        }
+    }
+}
---
+++ Глава 2. Классы. Объектно-ориентированное программирование/18. Абстрактные классы и члены классов/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Абстрактные_классы_и_члены_классов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
