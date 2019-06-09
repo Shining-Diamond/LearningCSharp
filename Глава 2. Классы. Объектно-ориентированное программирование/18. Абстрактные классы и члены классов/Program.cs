052ed05973411ac681cfcf6fdccc9435b618792e
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-09 11:55:13 +0300

    Абстрактные методы и классы

--- Глава 2. Классы. Объектно-ориентированное программирование/18. Абстрактные классы и члены классов/Program.cs
+++ Глава 2. Классы. Объектно-ориентированное программирование/18. Абстрактные классы и члены классов/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _18.Абстрактные_классы_и_члены_классов
 {
@@ -10,7 +6,106 @@ namespace _18.Абстрактные_классы_и_члены_классов
     {
         static void Main(string[] args)
         {
+            Client client = new Client("Игорь", 3000);
 
+            client.Display(); // Игорь
+            client.DispayAllInformation(); // Игорь: 3000
+
+            Employee employee = new Employee("Василий", "Менеджер");
+
+            employee.Display(); // Василий
+            employee.DispayAllInformation(); // Василий: Менеджер
+
+            Rectangle rectangle = new Rectangle(100, 50);
+
+            float area = rectangle.Area(); // 5000
+            float perimeter = rectangle.Perimeter(); // 300
+
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Person
+    {
+        public string Name { get; set; }
+ 
+        public Person(string name)
+        {
+            Name = name;
+        }
+ 
+        public void Display()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public abstract void DispayAllInformation();
+    }
+ 
+    class Client : Person
+    {
+        public int Sum { get; set; }    // сумма на счету
+ 
+        public Client(string name, int sum)
+            : base(name)
+        {
+            Sum = sum;
+        }
+
+        public override void DispayAllInformation()
+        {
+            Console.WriteLine($"{Name}: {Sum}");
+        }
+    }
+ 
+    class Employee : Person
+    {
+        public string Position { get; set; } // должность
+ 
+        public Employee(string name, string position) 
+            : base(name)
+        {
+            Position = position;
+        }
+
+        public override void DispayAllInformation()
+        {
+            Console.WriteLine($"{Name}: {Position}");
+        }
+    }
+
+    abstract class Figure
+    {
+        // абстрактный метод для получения периметра
+        public abstract float Perimeter();
+        // абстрактный метод для получения площади
+        public abstract float Area();
+    }
+
+    class Rectangle : Figure
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+ 
+        public Rectangle(float width, float height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        // переопределение получения периметра
+        public override float Perimeter()
+        {
+            return (Width + Height) * 2;
+        }
+
+        // переопределение получения площади
+        public override float Area()
+        {
+            return Width * Height;
         }
     }
 }
