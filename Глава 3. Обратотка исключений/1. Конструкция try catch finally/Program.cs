4786938928a263dcccea80b8371f1531a0164ca4
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-10 01:24:22 +0300

    Конструкция try..catch..finally

---
+++ Глава 3. Обратотка исключений/1. Конструкция try catch finally/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1.Конструкция_try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+
+            Console.WriteLine("Введите число");
+            int x2;
+            string input = Console.ReadLine();
+
+            if (Int32.TryParse(input, out x2))
+            {
+                x2 *= x2;
+                Console.WriteLine("Квадрат числа: " + x2);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
