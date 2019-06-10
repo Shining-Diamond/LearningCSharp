d0e5d9e4b27a9a12c1e279b93771390c301e4ee9
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-10 10:14:41 +0300

    Блок catch и фильтры исключений, Типы исключений. Класс Exception

---
+++ Глава 3. Обратотка исключений/2. Блок catch и фильтры исключений/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2.Блок_catch_и_фильтры_исключений
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 0;
+
+            try
+            {
+                int result = x / y;
+            }
+            catch (DivideByZeroException) when (y == 0)
+            {
+                Console.WriteLine("y не должен быть равен 0");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Глава 3. Обратотка исключений/3. Типы исключений. Класс Exception/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+/**
+ * InnerException: хранит информацию об исключении, которое послужило причиной текущего исключения
+ * Message: хранит сообщение об исключении, текст ошибки
+ * Source: хранит имя объекта или сборки, которое вызвало исключение
+ * StackTrace: возвращает строковое представление стека вызывов, которые привели к возникновению исключения
+ * TargetSite: возвращает метод, в котором и было вызвано исключение
+ */
+
+namespace _3.Типы_исключений.Класс_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+                Console.WriteLine($"Метод: {ex.TargetSite}");
+                Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+
+            try
+            {
+                int[] numbers = new int[4];
+                numbers[7] = 9;     // IndexOutOfRangeException
+
+                int x = 5;
+                int y = x / 0;  // DivideByZeroException
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение DivideByZeroException");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                object obj = "you";
+                int num = (int)obj;     // InvalidCastException
+                Console.WriteLine($"Результат: {num}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение DivideByZeroException");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Возникло исключение IndexOutOfRangeException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
