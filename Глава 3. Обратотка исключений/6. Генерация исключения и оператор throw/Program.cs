a0bf0ecf9ae0f17f26b672b7d76e23e4076d1ac3
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-11 22:13:55 +0300

    Генерация исключения и оператор throw

---
+++ Глава 3. Обратотка исключений/6. Генерация исключения и оператор throw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _6.Генерация_исключения_и_оператор_throw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Оператор throw переходит к внешнему блоку catch
+            try
+            {
+                try
+                {
+                    Console.Write("Введите строку: ");
+                    string message = Console.ReadLine();
+                    if (message.Length > 6)
+                    {
+                        throw new Exception("Длина строки больше 6 символов");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Возникло исключение");
+                    throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
