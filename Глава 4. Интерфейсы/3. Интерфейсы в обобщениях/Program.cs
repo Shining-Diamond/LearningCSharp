a03ed9d055269dbd4898ce26c0e7adbfc8c5bae2
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-13 23:32:22 +0300

    Интерфейсы в обобщениях

---
+++ Глава 4. Интерфейсы/3. Интерфейсы в обобщениях/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace _3.Интерфейсы_в_обобщениях
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client1 = new Client("Иван", 5000);
+            Client client2 = new Client("Василий", 4000);
+
+            Transaction<Client>.Operate(client1, client2, 1000);
+
+            Console.WriteLine($"Остаток на счету {client1.Name}: {client1.CurrentSum}\n\rОстаток на счету {client2.Name}: {client2.CurrentSum}");
+
+            // Обобщенный интерфейс
+            User<int> user = new User<int>(10);
+
+            Console.WriteLine(user.Id); // 10
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IAccount
+    {
+        int CurrentSum { get; }
+        void Put(int sum);
+        void Withdraw(int sum);
+    }
+
+    interface IClient
+    {
+        string Name { get; set; }
+    }
+
+    class Client : IAccount, IClient
+    {
+        int _sum;
+
+        public Client(string name, int sum)
+        {
+            Name = name;
+            _sum = sum;
+        }
+
+        public string Name { get; set; }
+
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum <= _sum)
+            {
+                _sum -= sum;
+            }
+        }
+    }
+
+    static class Transaction<T> where T : IAccount, IClient
+    {
+        public static void Operate(T account1, T account2, int sum)
+        {
+            if (account1.CurrentSum >= sum)
+            {
+                account1.Withdraw(sum);
+                account2.Put(sum);
+
+                Console.WriteLine($"Клиент {account1.Name} перевел {sum}$ {account2.Name}");
+            }
+        }
+    }
+
+    interface IUser<T>
+    {
+        T Id { get; }
+    }
+
+    class User<T> : IUser<T>
+    {
+        T _id;
+
+        public User(T id)
+        {
+            _id = id;
+        }
+
+        public T Id { get { return _id; } }
+    }
+}
---
+++ Глава 4. Интерфейсы/3. Интерфейсы в обобщениях/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("3. Интерфейсы в обобщениях")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3. Интерфейсы в обобщениях")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("102a2491-363f-4761-92b4-d2d21d8c0d89")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
