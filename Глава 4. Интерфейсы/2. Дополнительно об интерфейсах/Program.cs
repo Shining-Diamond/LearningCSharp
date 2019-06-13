6727c713c60c6744c01e69e5f7944724f0402a46
rGillyazanov <rgillyazanov@yandex.ru>
2019-06-13 13:36:48 +0300

    Дополнительно об интерфейсах

---
+++ Глава 4. Интерфейсы/2. Дополнительно об интерфейсах/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _2.Дополнительно_об_интерфейсах
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            ((ISchool)p).Study();
+            ((IUniversity)p).Study();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IAction
+    {
+        void Move();
+    }
+
+    // Наследование интерфейсов
+    interface IRunAction : IAction
+    {
+        void Run();
+    }
+
+    class BaseAction : IRunAction
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("Move");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Run");
+        }
+    }
+
+    // Изменение реализации интерфейса в производном классе
+
+    class HeroAction : BaseAction
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Move in HeroAction");
+        }
+    }
+
+    // Явное применение интерфейсов
+
+    class Person : ISchool, IUniversity
+    {
+        void ISchool.Study()
+        {
+            Console.WriteLine("Учеба в школе");
+        }
+        void IUniversity.Study()
+        {
+            Console.WriteLine("Учеба в университете");
+        }
+    }
+
+    interface ISchool
+    {
+        void Study();
+    }
+
+    interface IUniversity
+    {
+        void Study();
+    }
+}
