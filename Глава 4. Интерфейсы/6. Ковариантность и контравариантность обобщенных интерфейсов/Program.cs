b574199ea53779b7b77fa20f687d942b0c548233
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-08-05 23:06:22 +0300

    Ковариантность и контравариантность обобщенных интерфейсов

---
+++ Глава 4. Интерфейсы/6. Ковариантность и контравариантность обобщенных интерфейсов/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _6.Ковариантность_и_контравариантность_обобщенных_интерфейсов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBank<DepositAccount> depositBank = new Bank<DepositAccount>();
+            Account acc1 = depositBank.CreateAccount(34);
+
+            IBank<Account> ordinaryBank = new Bank<DepositAccount>();
+            Account acc2 = ordinaryBank.CreateAccount(45);
+
+            ITransaction<Account> accTransaction = new Transaction<Account>();
+            accTransaction.DoOperation(acc1, 400);
+
+            ITransaction<DepositAccount> depAccTransaction = new Transaction<Account>();
+            depAccTransaction.DoOperation((DepositAccount)acc2, 450);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Account
+    {
+        public virtual void DoTransfer(int sum)
+        {
+            Console.WriteLine($"Клиент положил на счет {sum} долларов");
+        }
+    }
+
+    class DepositAccount : Account
+    {
+        public override void DoTransfer(int sum)
+        {
+            Console.WriteLine($"Клиент положил на депозитный счет {sum} долларов");
+        }
+    }
+
+    interface IBank<out T>
+    {
+        T CreateAccount(int sum);
+    }
+
+    class Bank<T> : IBank<T> where T : Account, new()
+    {
+        public T CreateAccount(int sum)
+        {
+            T acc = new T();
+            acc.DoTransfer(sum);
+            return acc;
+        }
+    }
+
+    interface ITransaction<in T>
+    {
+        void DoOperation(T account, int sum);
+    }
+
+    class Transaction<T> : ITransaction<T> where T : Account
+    {
+        public void DoOperation(T account, int sum)
+        {
+            account.DoTransfer(sum);
+        }
+    }
+}
---
+++ Глава 4. Интерфейсы/6. Ковариантность и контравариантность обобщенных интерфейсов/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("6. Ковариантность и контравариантность обобщенных интерфейсов")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6. Ковариантность и контравариантность обобщенных интерфейсов")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7d06c315-db5f-4074-9a43-9f94db3b5652")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
