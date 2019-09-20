8cb17e67719fc3a5651147a56a7567db55d121af
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-09-20 16:50:32 +0300

    Локальные функции

---
+++ Глава 6. Дополнительные возможности ООП в C#/5. Локальные функции/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5.Локальные_функции
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetResult(new int[] { -3, -2, -1, 0, 1, 2, 3 });
+            Console.WriteLine(result);  // 6
+        }
+
+        static int GetResult(int[] numbers)
+        {
+            int limit = 0;
+            // локальная функция
+            bool IsMoreThan(int number)
+            {
+                return number > limit;
+            }
+
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (IsMoreThan(numbers[i]))
+                {
+                    result += numbers[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
