bb60f1cedf645a235ffb34d3cc84e834b9340c5c
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-09-20 21:09:15 +0300

    Деконструкторы

---
+++ Глава 6. Дополнительные возможности ООП в C#/7. Деконструкторы/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Деконструкторы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Tom",
+                Age = 23
+            };
+
+            (string name, int age) = person;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Можно применять с 2 и более параметрами
+        public void Deconstruct(out string name, out int age)
+        {
+            name = this.Name;
+            age = this.Age;
+        }
+    }
+}
