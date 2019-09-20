cc8d803f091335ba03e15933af4180f37a20587c
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-09-20 13:00:07 +0300

    Частичные классы и методы

---
+++ Глава 6. Дополнительные возможности ООП в C#/3. Частичные классы и методы/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _3.Частичные_классы_и_методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Move();
+            person.Eat();
+
+            person.DoSomething();
+        }
+    }
+
+    public partial class Person
+    {
+        public void Eat()
+        {
+            Console.WriteLine("I am eating");
+        }
+
+        partial void DoSomethingElse()
+        {
+            Console.WriteLine("I am reading a book");
+        }
+    }
+
+    public partial class Person
+    {
+        partial void DoSomethingElse();
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Start");
+            DoSomethingElse();
+            Console.WriteLine("Finish");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("I'm moving");
+        }
+    }
+}
