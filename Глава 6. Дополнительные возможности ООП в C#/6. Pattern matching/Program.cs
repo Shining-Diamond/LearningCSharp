45c2fdefe59ec9df1fd0d75785ae73893bf79b20
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-09-20 21:04:37 +0300

    Pattern matching

---
+++ Глава 6. Дополнительные возможности ООП в C#/6. Pattern matching/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _6.Pattern_matching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Manager();
+
+            UseEmployee(emp);
+        }
+
+        static void UseEmployee(Employee emp)
+        {
+            if (emp is Manager manager && !manager.IsOnVacation)
+            {
+                manager.Work();
+            }
+            else
+            {
+                Console.WriteLine("Преобразование не допустимо");
+            }
+        }
+    }
+
+    class Employee
+    {
+        public virtual void Work()
+        {
+            Console.WriteLine("Да работаю я, работаю");
+        }
+    }
+ 
+    class Manager : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Отлично, работаем дальше");
+        }
+
+        public bool IsOnVacation { get; set; }
+    }
+}
