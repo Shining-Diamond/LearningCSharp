20641442692f658781a6ba14d251bbf4cf775bbc
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-09-24 08:58:25 +0300

    Введение в коллекции

---
+++ Глава 8. Коллекции/1. Введение в коллекции/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace _1.Введение_в_коллекции
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // необобщенная коллекция ArrayList
+            ArrayList objectList = new ArrayList() { 1, 2, "string", 'c', 2.0f };
+ 
+            object obj = 45.8;
+ 
+            objectList.Add(obj);
+            objectList.Add("string2");
+            objectList.RemoveAt(0); // удаление первого элемента
+
+            foreach (object o in objectList)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine("Общее число элементов коллекции: {0}", objectList.Count);
+
+            // обобщенная коллекция List
+            List<string> countries = new List<string>() { "Россия", "США", "Великобритания", "Китай" };
+
+            countries.Add("Франция");
+            countries.RemoveAt(1); // удаление второго элемента
+
+            foreach (string s in countries)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
