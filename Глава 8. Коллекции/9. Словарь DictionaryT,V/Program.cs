9c9800f17943f0aa1902ab577fa0a088a1f05109
Ruslan Gillyazanov <50686016+rGillyazanov@users.noreply.github.com>
2019-09-25 10:19:22 +0300

    Словарь Dictionary<T, V>

---
+++ Глава 8. Коллекции/9. Словарь DictionaryT,V/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9.Словарь_DictionaryT_V
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> countries = new Dictionary<int, string>(5);
+            countries.Add(1, "Russia");
+            countries.Add(3, "Great Britain");
+            countries.Add(2, "USA");
+            countries.Add(4, "France");
+            countries.Add(5, "China");
+
+            foreach (KeyValuePair<int, string> keyValue in countries)
+            {
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
+
+            // получение элемента по ключу
+            string country = countries[4];
+            // изменение объекта
+            countries[4] = "Spain";
+            // удаление по ключу
+            countries.Remove(2);
+
+            Dictionary<char, Person> people = new Dictionary<char, Person>();
+            people.Add('b', new Person() { Name = "Bill" });
+            people.Add('t', new Person() { Name = "Tom" });
+            people.Add('j', new Person() { Name = "John" });
+
+            foreach (KeyValuePair<char, Person> keyValue in people)
+            {
+                // keyValue.Value представляет класс Person
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value.Name);
+            }
+
+            // перебор ключей
+            foreach (char c in people.Keys)
+            {
+                Console.WriteLine(c);
+            }
+
+            // перебор по значениям
+            foreach (Person p in people.Values)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
